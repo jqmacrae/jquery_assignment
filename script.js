db98e1ac72aae941ca75b20a8c441626a65b2f22
Jeremy Macrae <jqmacrae@Jeremys-MacBook-Pro.local>
2019-01-30 12:21:42 -0800

    slowly but surely

---
+++ script.js
@@ -0,0 +1,17 @@
+
+$ (document) .ready(function() {
+
+    // $( "p" ).click(function() {
+    //     $( this ).slideUp();
+    //   });
+      
+      $(".bluebtn").click(function() {
+          $( "p1" ).slideToggle();
+        });
+
+        // $( ".bluebtn" ).click(function() {
+        //     $( "p1" ).css("color", "red");
+        //   });
+
+
+})
